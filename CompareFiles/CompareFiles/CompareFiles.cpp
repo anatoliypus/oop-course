a586fb695b07074e0792fe943ab22797a924fba1
Anatoliy <anatoliypuss5@gmail.com>
2021-02-18 21:38:53 +0300

    Fixed issues with CompareFiles

--- CompareFiles/CompareFiles/CompareFiles.cpp
+++ CompareFiles/CompareFiles/CompareFiles.cpp
@@ -1,51 +1,78 @@
-﻿#include <iostream>
-#include <fstream>
+﻿#include <fstream>
+#include <iostream>
 #include <string>
 
+struct Args {
+  std::string file1;
+  std::string file2;
+};
+
+struct Error {
+  std::string message;
+};
+
+bool ParseArgs(int argc, char* argv[], Args& args, Error& err) {
+  if (argc != 3) {
+    err.message = "Wrong arguments given\n";
+    return false;
+  }
+  args.file1 = argv[1];
+  args.file2 = argv[2];
+  return true;
+}
+
+int checkFiles(std::ifstream& file1, std::ifstream& file2, Error& err) {
+  std::string str1, str2;
+  unsigned int lineCounter = 0;
+  bool equal = true;
+
+  while (std::getline(file1, str1) && std::getline(file2, str2)) {
+    ++lineCounter;
+    if (str1 != str2) {
+      equal = false;
+      break;
+    }
+  }
+
+  if (file1.bad() || file2.bad()) {
+    err.message = "Failed to read files \n";
+    return -1;
+  }
+
+  if (equal) {
+    return 0;
+  } else {
+    return lineCounter;
+  }
+}
+
 int main(int argc, char* argv[]) {
-	if (argc != 3)
-	{
-		std::cout << "Wrong parameters given\n";
-		return 1;
-	}
+  Error err;
+  Args args;
+  if (!ParseArgs(argc, argv, args, err)) {
+    std::cout << err.message;
+    return 1;
+  }
 
-	std::ifstream file1, file2;
-	file1.open(argv[1]);
-	file2.open(argv[2]);
+  std::ifstream file1, file2;
+  file1.open(args.file1);
+  file2.open(args.file2);
 
-	if (!file1.is_open() || !file2.is_open()) {
-		std::cout << argv[1] << std::endl;
-		std::cout << "Failed to read files data";
-		return 1;
-	}
+  if (!file1.is_open() || !file2.is_open()) {
+    std::cout << "Failed to read files data";
+    return 1;
+  }
 
-	std::string str1, str2;
-	unsigned int lineCounter = 0;
-	bool equal = true;
+  int result = checkFiles(file1, file2, err);
 
-	while (getline(file1, str1) && getline(file2, str2))
-	{
-		++lineCounter;
-		if (str1 != str2) {
-			equal = false;
-			break;
-		}
-	}
+  if (result == 0) {
+    std::cout << "Files are equal\n";
+  } else if (result < 0) {
+    std::cout << err.message;
+    return 1;
+  } else {
+    std::cout << "Files are different. Line number is " << result << std::endl;
+  }
 
-	if (file1.bad() || file2.bad()) 
-	{
-		std::cout << "Failed to read files \n";
-		return 1;
-	}
-
-	if (equal)
-	{
-		std::cout << "Files are equal\n";
-	}
-	else
-	{
-		std::cout << "Files are different. Line number is " << lineCounter << std::endl;
-	}
-
-	return 0;
+  return 0;
 }
